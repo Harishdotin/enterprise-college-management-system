ed7cc258966050a9cf89922d1e11e02f4ceeacb3
Harishdotin <harishkathiresan18@gmail.com>
2026-07-05 15:32:00 +0530

    Configure PostgreSQL for Render

--- CollegeManagementSystem/settings.py
+++ CollegeManagementSystem/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 import dj_database_url
@@ -95,10 +95,9 @@ WSGI_APPLICATION = 'CollegeManagementSystem.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
