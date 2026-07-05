2b743b0ecdbd2e3b900f078d459eb7ccc676a27f
Harishdotin <harishkathiresan18@gmail.com>
2026-07-05 16:35:21 +0530

    create default superuser for production

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import User
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+def ready(self):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@gmail.com",
+            password="admin123"
+        )
+
